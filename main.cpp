440fdd287c64be8ee700b54fc7004a2f50dbba9c
Enrique Samson Jr <enrique.samson@phitopolis.com>
2020-07-15 08:16:01 +0800

    add thread pool; use buffer_body for progress callback; remove timeout on connect

--- main.cpp
+++ main.cpp
@@ -1,30 +1,31 @@
 #include <cstdlib>
-#include <memory>
+#include <vector>
+#include <thread>
 #include "session.hpp"
 
 
 int main(int argc, char** argv)
 {
-    // Check command line arguments.
-    if(argc != 4 && argc != 5)
+  // Check command line arguments.
+  if(argc != 4 && argc != 5)
     {
-        std::cerr <<
-            "Usage: http-client-async <host> <port> <target> [<HTTP version: 1.0 or 1.1(default)>]\n" <<
-            "Example:\n" <<
-            "    http-client-async www.example.com 80 /\n" <<
-            "    http-client-async www.example.com 80 / 1.0\n";
-        return EXIT_FAILURE;
+      std::cerr <<
+	"Usage: http-client-async <host> <port> <target> [<HTTP version: 1.0 or 1.1(default)>]\n" <<
+	"Example:\n" <<
+	"    http-client-async www.example.com 80 /\n" <<
+	"    http-client-async www.example.com 80 / 1.0\n";
+      return EXIT_FAILURE;
     }
-    auto const host = argv[1];
-    auto const port = argv[2];
-    auto const target = argv[3];
-    int version = argc == 5 && !std::strcmp("1.0", argv[4]) ? 10 : 11;
-
-    // The io_context is required for all I/O
-    net::io_context ioc;
-
-    // Launch the asynchronous operation
-    for (int n=0; n<10; ++n)
+  auto const host = argv[1];
+  auto const port = argv[2];
+  auto const target = argv[3];
+  int version = argc == 5 && !std::strcmp("1.0", argv[4]) ? 10 : 11;
+  
+  // The io_context is required for all I/O
+  net::io_context ioc;
+  
+  // Launch the asynchronous operation
+  for (int n=0; n<10; ++n)
     {
       std::ostringstream os;
       os << target
@@ -33,10 +34,20 @@ int main(int argc, char** argv)
 	 << "_20M.csv";
       std::make_shared<session>(ioc)->run(host, port, os.str().c_str(), version);
     }
-
-    // Run the I/O service. The call will return when
-    // the get operation is complete.
-    ioc.run();
-
-    return EXIT_SUCCESS;
+  
+  // Run the I/O service. The call will return when
+  // the get operation is complete.
+  std::vector<std::thread> tvec;
+  for (int t=0; t<2; ++t)
+    {
+      tvec.emplace_back([&ioc] { ioc.run(); });
+    }
+  //ioc.run();
+  
+  for (auto & thr : tvec)
+    {
+      thr.join();
+    }
+  
+  return EXIT_SUCCESS;
 }
